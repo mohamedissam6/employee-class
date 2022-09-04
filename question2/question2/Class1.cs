a248fb2ed7e166d450f8bc972969f5a9fcbedd03
mohamedissam6 <112808440+mohamedissam6@users.noreply.github.com>
2022-09-04 20:40:57 +0200

    Add files via upload

---
+++ question2/question2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question2
+{
+    internal class employee
+    {
+        private int Number;
+        private string Name;
+        private DateTime DateofhireDate;
+        private int Salary;
+
+        public int number { get; set; }
+        public string name { get; set; }
+        public DateTime dateofhireDate { get; set; }
+        public int salary { get; set; }
+
+
+        public employee (int number, string name, DateTime dateofhireDate, int salary)
+        {
+            Number = number;
+            Name = name;
+            DateofhireDate = dateofhireDate;
+            Salary = salary;
+        }
+
+        public string ConvertString()
+        {
+            return ($"Number : {Number}\nName : {Name}\nDate of hire : {DateofhireDate}\nSalary : {Salary}");
+        }
+
+        public double CalculateTax()
+        {
+            return (0.1*(Convert.ToDouble(Salary)));
+        }
+
+    }
+}
---
+++ question2/question2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee emp = new employee(1, "aa", new DateTime(2005, 5, 5), 2500);
+            Console.WriteLine(emp.ConvertString());
+            
+            Console.WriteLine(emp.CalculateTax());
+            Console.ReadLine();
+
+
+        }
+    }
+}
